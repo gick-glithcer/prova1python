3e43417ecf51eeb38caf5cea3ad6f1e7f3a4ee2a
gick-glithcer <samuelffdima2@gmail.com>
2026-04-01 10:46:14 -0300

    Add files via upload

---
+++ verificador.py
@@ -0,0 +1,7 @@
+#Login
+usuario = input("Digite o Usuário:")
+if usuario == "admim":
+    print("Acesso Total Liberado✅")
+else:
+    print("Acesso Totoal Limitado⛔")
+#--Samuel Henrique Das Chagas--
